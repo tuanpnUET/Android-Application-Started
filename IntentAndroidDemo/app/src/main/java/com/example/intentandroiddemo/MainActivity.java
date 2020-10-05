84135b19b3f98f8d1a3a3b9eb945347a27b87174
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-10-05 19:03:34 +0700

    Update MainActivity.java

--- IntentAndroidDemo/app/src/main/java/com/example/intentandroiddemo/MainActivity.java
+++ IntentAndroidDemo/app/src/main/java/com/example/intentandroiddemo/MainActivity.java
@@ -1,14 +1,30 @@
 package com.example.intentandroiddemo;
 
+import android.content.Intent;
+import android.net.Uri;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import java.net.URI;
 
 public class MainActivity extends AppCompatActivity {
-
-@Override
-protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ImageView imgChorme;
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_main);
+                mapping();
         }
+        public void openChrome(View view){
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("https://www.youtube.com/watch?v=329d75wpmW8"));
+                startActivity(intent);
         }
+        private void mapping(){
+                imgChorme = findViewById(R.id.imgChorme);
+        }
+}
