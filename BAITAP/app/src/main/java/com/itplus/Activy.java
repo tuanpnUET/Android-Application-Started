f2d076b562bac86aef0ebde2eba5510fce257117
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-10-05 19:17:52 +0700

    Create Activy.java

---
+++ BAITAP/app/src/main/java/com/itplus/Activy.java
@@ -0,0 +1,14 @@
+package com.itplus;
+
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class Activy extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bai12);
+    }
+}
