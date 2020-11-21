d6208e4d003a8bc5d6c64b64085b249cb2288875
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-11-21 14:44:17 +0700

    Create WeatherAdapter.java

---
+++ Weather Predicted App/app/src/main/java/com/weatherpredictedapp/WeatherAdapter.java
@@ -0,0 +1,40 @@
+package com.weatherpredictedapp;
+
+import android.content.Context;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+
+import java.util.List;
+
+public class WeatherAdapter extends BaseAdapter {
+    Context context;
+    int layout;
+    List<Weather> list;
+
+    public WeatherAdapter(Context context, int layout, List<Weather> list) {
+        this.context = context;
+        this.layout = layout;
+        this.list = list;
+    }
+
+    @Override
+    public int getCount() {
+        return list.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return null;
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        return null;
+    }
+}
