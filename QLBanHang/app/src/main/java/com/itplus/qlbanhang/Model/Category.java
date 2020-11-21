3c42fd5a3de5c13090e4f92df879bf1974c08cff
tuanpn.tb <45645698+tuanpnUET@users.noreply.github.com>
2020-11-21 14:47:21 +0700

    Create Category.java

---
+++ QLBanHang/app/src/main/java/com/itplus/qlbanhang/Model/Category.java
@@ -0,0 +1,40 @@
+package com.itplus.qlbanhang.Model;
+
+public class Category {
+    private int id;
+    private String name;
+    private String avatar;
+
+    public Category() {
+    }
+
+    public Category(int id, String name, String avatar) {
+        this.id = id;
+        this.name = name;
+        this.avatar = avatar;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAvatar() {
+        return avatar;
+    }
+
+    public void setAvatar(String avatar) {
+        this.avatar = avatar;
+    }
+}
